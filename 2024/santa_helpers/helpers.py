cfc499b0d3dc058b308ee616fee2061d070a0e2a
FrederickFranck <frederick_spore@hotmail.com>
2024-12-03 17:23:49 +0100

    added timing helper functions

--- 2024/santa_helpers/helpers.py
+++ 2024/santa_helpers/helpers.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import inspect
+import time
 
 
 def read_input(demo=True):
@@ -13,3 +14,11 @@ def read_input(demo=True):
     with open(filename, "r") as file:
         content = file.read()
     return content
+
+
+def _start():
+    return time.perf_counter()
+
+
+def _stop(start):
+    print(f"took {(time.perf_counter()-start)*1000} ms")
